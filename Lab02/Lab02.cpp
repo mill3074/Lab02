d25b2c50fb06067958bed60558f3aa6855845bc2
mill3074 <mill3074@purdue.edu>
2024-08-28 15:02:01 -0400

    okay now finished

--- Lab02/Lab02.cpp
+++ Lab02/Lab02.cpp
@@ -9,45 +9,35 @@ int main()
     float B;
     float X;
 
-    cout << "Welcome to the Awesome Calculation Evaluator (A.C.E)" << endl;
+    cout << "Welcome to the Amazing Calculation Emitter (A.C.E)" << endl;
 
-    cout << "Solve Ax + B = 0 for x" << endl;
+    cout << "Let's solve Ax + B = 0 for x" << endl;
     cout << " " << endl;
 
     cout << "Please enter a value for A: ";
     cin >> A;
 
     //I just added these for fun, you can disregard this
-    if (A > 1000000) {
+    if (A > 100000) {
         cout << "Nice Keyboard Slam." << endl;
         cout << " " << endl;
     }
-    if (A < 0.9999999) {
-        cout << "Nice Keyboard Slam." << endl;
-        cout << " " << endl;
-    }
-
-    cout << A << "x + B = 0" << endl;
-    cout << " " << endl;
 
     cout << "Please enter a value for B: ";
     cin >> B;
+    cout << " " << endl;
 
     //I just added these for fun, you can disregard this
-    if (B > 1000000) {
-        cout << "Nice Keyboard Slam." << endl;
-        cout << " " << endl;
-    }
-    if (B < 0.9999999) {
+    if (B > 100000) {
         cout << "Nice Keyboard Slam." << endl;
         cout << " " << endl;
     }
 
-    cout << A << "x + " << B << " = 0" << endl;
+    cout << "If " << A << "x + " << B << " = 0..." << endl;
     cout << " " << endl;
 
     X = -B/A;
-    cout << "Then X= " << X << endl;
+    cout << "...then X= " << X << "!" << endl;
 }
 
 
